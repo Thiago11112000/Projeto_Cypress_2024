0e23e5f55d9673b0a50ab443336ab89f2a54169e
Thiago Silva <thiagoclick2000@hotmail.com>
2024-02-01 09:19:58 -0300

    refactoring Register_Page_Commands.js adding an object of elements

--- cypress/support/Register_Page_Commands.js
+++ cypress/support/Register_Page_Commands.js
@@ -1,34 +1,51 @@
 /// <reference types = "cypress"/>
 
+const elements = {
+    buttons: {
+      register:  '#btnRegister'
+    },
+    fields: {
+    name: '#user',
+    email: '#email',
+    password: '#password'
+    },
+    messages: {
+     error: '.errorLabel',
+     sucessTitle: '#swal2-title',
+     sucessSubTitle: '#swal2-html-container'
+    }
+
+}
+
 
 Cypress.Commands.add('saveRegister', () =>{
-    cy.get('#btnRegister')
+    cy.get(elements.buttons.register)
     .click()
 })
 
 
 Cypress.Commands.add('fillEmail', (email) =>{
-    cy.get('#email')
+    cy.get(elements.fields.email)
     .type(email)
 })
 Cypress.Commands.add('fillName', (name) =>{
-    cy.get('#user')
+    cy.get(elements.fields.name)
     .type(name)
 })
 Cypress.Commands.add('fillPassword', (password) =>{
-    cy.get('#password')
+    cy.get(elements.fields.password)
     .type(password)
 })
 
 
 Cypress.Commands.add('checkMessage', (message) =>{ 
-    cy.get('.errorLabel')
+    cy.get(elements.messages.error)
     .should('have.text', message)    
 })
 Cypress.Commands.add('checkRegisterSuccess', (name) =>{ 
-    cy.get('#swal2-title')
+    cy.get(elements.messages.sucessTitle)
     .should('have.text','Cadastro realizado!')
    
-   cy.get('#swal2-html-container')
+   cy.get(elements.messages.sucessSubTitle)
     .should('have.text',`Bem-vindo ${name}`)
 })
